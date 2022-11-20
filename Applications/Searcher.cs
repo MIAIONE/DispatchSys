3b05a97eea26ced02efcf81066e8c14ed0247eb4
MIAIONE <MIAIONE@Yeah.net>
2022-11-20 17:30:51 +0800

    添加项目文件。

---
+++ Applications/Searcher.cs
@@ -0,0 +1,9 @@
+﻿namespace DispatchSys.Applications;
+
+public class Searcher
+{
+    public void Submit(string text)
+    {
+        File.AppendAllText(@$"./logs_{DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")}.log", text);
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DispatchSys.Applications;
+using DispatchSys.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DispatchSys.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly Searcher _searcher;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        _searcher = new();
+    }
+
+    public IActionResult Index(string name)
+    {
+        _searcher.Submit(name);
+        _logger.LogInformation("User input = {Name}", name);
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        _logger.LogError("RequestId = {message}", Activity.Current?.Id ?? HttpContext.TraceIdentifier);
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
